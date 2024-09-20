a2442bbf3b4326e8a03d378ea25ac2654fe38a0b
AriRuizMartinez <ariruizmartinez@gmail.com>
2024-09-20 11:11:54 +0200

    Pruebas en el main

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -16,6 +16,25 @@ namespace Herencia
             Rombo rombo = new Rombo(10, 5);
             Cuadrado cuadrado = new Cuadrado(5);
             Rectangulo rectangulo = new Rectangulo(3, 5);
+
+            Console.WriteLine(elipse.ToString());
+            Console.WriteLine("Area: " + elipse.GetArea());
+            Console.WriteLine("Perimetro: " + elipse.GetPerimetro());
+            Console.WriteLine(circulo.ToString());
+            Console.WriteLine("Area: " + circulo.GetArea());
+            Console.WriteLine("Perimetro: " + circulo.GetPerimetro());
+            Console.WriteLine(triangulo.ToString());
+            Console.WriteLine("Area: " + triangulo.GetArea());
+            Console.WriteLine("Perimetro: " + triangulo.GetPerimetro());
+            Console.WriteLine(rombo.ToString());
+            Console.WriteLine("Area: " + rombo.GetArea());
+            Console.WriteLine("Perimetro: " + rombo.GetPerimetro());
+            Console.WriteLine(cuadrado.ToString());
+            Console.WriteLine("Area: " + cuadrado.GetArea());
+            Console.WriteLine("Perimetro: " + cuadrado.GetPerimetro());
+            Console.WriteLine(rectangulo.ToString());
+            Console.WriteLine("Area: " + rectangulo.GetArea());
+            Console.WriteLine("Perimetro: " + rectangulo.GetPerimetro());
         }
     }
 }
